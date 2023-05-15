9be69b5b79edb528a2820dce98ed7da989582675
Sérgio Filho <sergiovf_@hotmail.com>
2023-05-15 17:22:44 -0300

    fix(path): fix data paths

--- code/utils.py
+++ code/utils.py
@@ -4,5 +4,5 @@ from pathlib import Path
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 path = {"root": Path(__file__).parent.parent.absolute()}
-path["data"] = Path("/mnt/f/respyra")
-path["psysym"] = path["root"] / "data" / "PsySym"
+path["data"] = path["root"] / "data"
+path["psysym"] = path["data"] / "PsySym"
