1e836f50b8d0ac78a1f82b36207a69b431c15e03
Sérgio Filho <sergiovf_@hotmail.com>
2023-06-15 15:00:36 -0300

    fix: remove dataset duplication

--- code/1_concat_dataset.py
+++ code/1_concat_dataset.py
@@ -1,29 +1,16 @@
 import pandas as pd
 from utils import path
 
-train_1 = pd.read_csv(path["psysym"] / "data" / "symp_data" / "train.csv")
-valid_1 = pd.read_csv(path["psysym"] / "data" / "symp_data" / "val.csv")
-test_1 = pd.read_csv(path["psysym"] / "data" / "symp_data" / "test.csv")
+train = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "train.csv")
+valid = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "val.csv")
+test = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "test.csv")
 
-train_0 = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "train.csv")
-valid_0 = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "val.csv")
-test_0 = pd.read_csv(path["psysym"] / "data" / "symp_data_w_control" / "test.csv")
+train["type"] = "train"
+valid["type"] = "valid"
+test["type"] = "test"
 
-train_1["type"] = "train"
-valid_1["type"] = "valid"
-test_1["type"] = "test"
+posts = pd.concat([train, valid, test])
 
-train_0["type"] = "train"
-valid_0["type"] = "valid"
-test_0["type"] = "test"
-
-pos_label = pd.concat([train_1, valid_1, test_1])
-neg_label = pd.concat([train_0, valid_0, test_0])
-
-pos_label["classification"] = 1
-neg_label["classification"] = 0
-
-posts = pd.concat([pos_label, neg_label])
 posts = posts.sample(frac=1, random_state=42).reset_index(drop=True)
 
 posts.to_parquet(path["data"] / "full.snappy.parquet")
